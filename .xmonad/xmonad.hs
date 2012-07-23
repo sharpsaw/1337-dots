aae4d3898707b1b4af093bc84b62dab48c282aa4
☈king <rking@sharpsaw.org>
2012-07-23 07:46:31 -0600

    workspaces/manageHook work. Also sad modMask.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,22 +8,35 @@ import XMonad.Layout.NoBorders
 import System.Process
 import System.Exit
 
+myWorkspaces = [
+    "1:dev","2:web","3:irc","4:skype","5:misc",
+    "6:emu","7:chrome","8:ie8","9","0","-","="]
+
+myManageHook = composeAll
+    [ className =? "Mumble" --> doFloat
+    , className =? "Pentadactyl" --> doShift "2:web" -- not matching
+    , className =? "Skype" --> doShift "4:skype"
+    ]
+
 myLayout = ThreeCol 1 (3/100) (1/2) ||| ThreeColMid 1 (3/100) (1/2)
     ||| Full
     ||| noBorders (tabbed shrinkText defaultTheme)
     ||| Accordion
 
 -- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
-myModMask = do retval <- system "xmodmap | grep '^mod3\\s*\\S*' >/dev/null"
-               if retval == ExitSuccess
-                 then return mod3Mask
-                 else return mod1Mask
+-- myModMask = do retval <- system "xmodmap | grep '^mod3\\s*\\S*' >/dev/null"
+--                if retval == ExitSuccess
+--                  then return mod3Mask
+--                  else return mod1Mask
+myModMask = mod3Mask
 
 -- TODO: Also make this conditional (on there being a working urxvt.)
 myTerminal = "LANG=en_US.utf8 urxvt"
 
 main = xmonad defaultConfig
-            { layoutHook = myLayout
+            { workspaces = myWorkspaces
+            , layoutHook = myLayout
+            , manageHook = myManageHook <+> manageHook defaultConfig
             , modMask = myModMask
             , terminal = myTerminal
             , normalBorderColor  = "black"
