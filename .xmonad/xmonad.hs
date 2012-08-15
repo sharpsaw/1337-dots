dd25c9080dc180d149afb73e99ebd612ec687fea
☈king <rking@sharpsaw.org>
2012-08-15 10:32:54 -0600

    Starting extra-automation stuff. TODO though.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,37 @@
+-- Docs: http://xmonad.org/xmonad-docs/frames.html
+-- Exploring:- ghci ~/.xmonad/xmonad.hs
+
 import XMonad
 
---- Note: This requires xmonad-contrib (on Gentoo, at least)
+-- Note: These require xmonad-contrib
 import XMonad.Layout.ThreeColumns
-import XMonad.Layout.Tabbed
-import XMonad.Layout.Accordion
-import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig
+
+import qualified Data.Map as M
+
 import System.Process
 import System.Exit
+
+main = xmonad $ defaultConfig
+    { workspaces = myWorkspaces
+    , layoutHook = myLayout
+    , manageHook = myManageHook <+> manageHook defaultConfig
+    , modMask = myModMask
+    , terminal = myTerminal
+    , normalBorderColor  = "black"
+    , focusedBorderColor = "green"
+    , borderWidth = 1
+    }
+    -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html
+    -- and:
+    -- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Types.html
+    `additionalKeys`
+    [ ((mod1Mask, xK_F5 ), spawn "~/bin/Alt+F5")
+    , ((mod1Mask, xK_F9 ), spawn "~/bin/Alt+F9")
+    ]
+    `additionalKeysP`
+    [ ("C-M-l", spawn "~/bin/,lock" ) -- from https://github.com/sharpsaw/x-dots/blob/master/bin/%2Clock
+    ]
 
 myWorkspaces = [
     "1:dev","2:web","3:irc","4:skype","5:misc",
@@ -21,22 +46,13 @@ myManageHook = composeAll
 myLayout = ThreeCol 1 (3/100) (1/2) ||| Full
 
 -- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
--- myModMask = do retval <- system "xmodmap | grep '^mod3\\s*\\S*' >/dev/null"
---                if retval == ExitSuccess
---                  then return mod3Mask
---                  else return mod1Mask
+-- Needs another approach:
+-- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Xlib-Extras.html#v:getModifierMappi
+{-myModMask = do retval <- system "xmodmap | grep '^mod3\\s*\\S*' >/dev/null"-}
+               {-if retval == ExitSuccess-}
+                 {-then return mod3Mask-}
+                 {-else return mod1Mask-}
 myModMask = mod3Mask
 
 -- TODO: Also make this conditional (on there being a working urxvt.)
 myTerminal = "LANG=en_US.utf8 urxvt"
-
-main = xmonad defaultConfig
-            { workspaces = myWorkspaces
-            , layoutHook = myLayout
-            , manageHook = myManageHook <+> manageHook defaultConfig
-            , modMask = myModMask
-            , terminal = myTerminal
-            , normalBorderColor  = "black"
-            , focusedBorderColor = "green"
-            , borderWidth = 1
-            }
