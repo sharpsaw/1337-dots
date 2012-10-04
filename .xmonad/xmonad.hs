0837d03f41f0eee827489b17905a09bf01fd65d2
☈king <rking@sharpsaw.org>
2012-10-04 18:08:47 -0500

    Fix so that mod1Mask is available as fallback.
    Took about fifty guys from #haskell to get to the bottom of this. (Ok,
    about 8, but that "8" is actually not hyperbole.)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,27 +14,45 @@ import qualified Data.Map as M
 
 import System.Process
 import System.Exit
+import System.Posix.Signals
 
-main = xmonad $ defaultConfig
-    { workspaces = myWorkspaces
-    , layoutHook = myLayout
-    , manageHook = myManageHook <+> manageHook defaultConfig
-    , modMask = myModMask
-    , terminal = myTerminal
-    , normalBorderColor  = "black"
-    , focusedBorderColor = "green"
-    , borderWidth = 1
-    }
-    -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html
-    -- and:
-    -- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Types.html
-    `additionalKeys`
-    [ ((mod1Mask, xK_F5 ), spawn "~/bin/Alt+F5")
-    , ((mod1Mask, xK_F9 ), spawn "~/bin/Alt+F9")
-    ]
-    `additionalKeysP`
-    [ ("C-M-l", spawn "~/bin/,lock" ) -- from https://github.com/sharpsaw/x-dots/blob/master/bin/%2Clock
-    ]
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+
+-- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
+-- Needs another approach:
+-- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Xlib-Extras.html#v:getModifierMappi
+determineModMask = do
+    r <- system "xmodmap | grep '^mod3\\s*\\S' >/dev/null"
+    return (if r == ExitSuccess then mod3Mask else mod1Mask)
+
+main = do
+    installHandler processStatusChanged Default Nothing
+    myModMask <- determineModMask
+    print mod3Mask
+    print mod1Mask
+    print myModMask -- TODO: see what this picks when mod3's not selected
+    xmonad $ defaultConfig
+        { workspaces = myWorkspaces
+        , layoutHook = myLayout
+        , manageHook = myManageHook <+> manageHook defaultConfig
+        , modMask = myModMask
+        , terminal = myTerminal
+        , normalBorderColor  = "black"
+        , focusedBorderColor = "green"
+        , borderWidth = 1
+        }
+        -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html
+        -- and:
+        -- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Types.html
+        `additionalKeys`
+        [ ((mod1Mask, xK_F5 ), spawn "~/bin/Alt+F5")
+        , ((mod1Mask, xK_F9 ), spawn "~/bin/Alt+F9")
+        , ((myModMask, xK_p), shellPrompt defaultXPConfig)
+        ]
+        `additionalKeysP`
+        [ ("C-M-l", spawn "~/bin/,lock" ) -- from https://github.com/sharpsaw/x-dots/blob/master/bin/%2Clock
+        ]
 
 myWorkspaces = [
     "1:dev","2:web","3:irc","4:skype","5:misc",
@@ -47,15 +65,6 @@ myManageHook = composeAll
     ]
 
 myLayout = ThreeCol 1 (3/100) (1/2) ||| Full
-
--- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
--- Needs another approach:
--- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Xlib-Extras.html#v:getModifierMappi
-{-myModMask = do retval <- system "xmodmap | grep '^mod3\\s*\\S*' >/dev/null"-}
-               {-if retval == ExitSuccess-}
-                 {-then return mod3Mask-}
-                 {-else return mod1Mask-}
-myModMask = mod3Mask
 
 -- TODO: Also make this conditional (on there being a working urxvt.)
 myTerminal = "LANG=en_US.utf8 urxvt"
