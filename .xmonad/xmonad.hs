90a2008690dd951c26e3b609be4f5da5f1198e57
☈king <rking@sharpsaw.org>
2013-02-08 17:38:00 -0600

    Refactoring xmonad.hs (though I don't use it, now)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,26 +3,23 @@
 
 import XMonad
 
--- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Actions-TopicSpace.html
--- TODO: import XMonad.Actions.TopicSpace
-
--- Note: These require xmonad-contrib
+--- Note: These require xmonad-contrib
 import XMonad.Layout.ThreeColumns
 import XMonad.Util.EZConfig
 
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
-import System.Process
-import System.Exit
-import System.Posix.Signals
+keysForAltFn =
+  [ ((mod1Mask, xK_F5 ), spawn "~/bin/Alt+F5")
+  , ((mod1Mask, xK_F9 ), spawn "~/bin/Alt+F9") ]
 
-import XMonad.Prompt
+keysToBeLikeOSX =
+  [ ("M-x", spawn "notify-send woohoo")
+  ]
 
--- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
-determineModMask = do
-    r <- system "xmodmap | grep '^mod3\\s*\\S' >/dev/null"
-    return (if r == ExitSuccess then mod3Mask else mod1Mask)
+
+keyToLockScreen = [ ("C-M-l", spawn "~/bin/,lock" ) ]
 
 keysToMoveWindowAndSwitchWorkspaces =
         [ ("M-"++m++show k, f i)
@@ -32,45 +29,88 @@ keysToMoveWindowAndSwitchWorkspaces =
              (\i -> windows (W.shift i) >> windows (W.greedyView i), "S-")]
         ]
 
--- from https://github.com/sharpsaw/x-dots/blob/master/bin/%2Clock
-keyToLockScreen = [ ("C-M-l", spawn "~/bin/,lock" ) ]
+myWorkspaces = [
+    "1:dev","2:web","3:chat","4:mail","5:voip",
+    "6:misc","7:misc2","8:dev2","9:misc3","0","-","=" ]
 
-keysForAltFn =
-  [ ((mod1Mask, xK_F5 ), spawn "~/bin/Alt+F5")
-  , ((mod1Mask, xK_F9 ), spawn "~/bin/Alt+F9") ]
+myLayout = ThreeCol 1 (3/100) (1/2) ||| Full
+
+-- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Actions-TopicSpace.html
+-- TODO: import XMonad.Actions.TopicSpace
 
 main = do
-    installHandler processStatusChanged Default Nothing
-    myModMask <- determineModMask
     xmonad $ defaultConfig
-        { workspaces = myWorkspaces
-        , layoutHook = myLayout
-        , manageHook = myManageHook <+> manageHook defaultConfig
-        , modMask = myModMask
+        { modMask = mod4Mask
         , terminal = myTerminal
+        , layoutHook = myLayout
         , normalBorderColor  = "black"
         , focusedBorderColor = "green"
-        , borderWidth = 1
+        , borderWidth = 2
         }
         -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html
         -- and:
         -- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Types.html
         `additionalKeys`
-        keysForAltFn
+        (keysForAltFn)
         `additionalKeysP`
-        (keyToLockScreen ++ keysToMoveWindowAndSwitchWorkspaces)
+        (keyToLockScreen ++ keysToBeLikeOSX ++ keysToMoveWindowAndSwitchWorkspaces)
 
-myWorkspaces = [
-    "1:dev","2:web","3:irc","4:skype","5:misc",
-    "6:emu","7:chrome","8:ie8","9","0","-","="]
-
-myManageHook = composeAll
-    [ className =? "Mumble" --> doFloat
-    , className =? "Pentadactyl" --> doShift "2:web" -- not matching
-    , className =? "Skype" --> doShift "4:skype"
-    ]
-
-myLayout = ThreeCol 1 (3/100) (1/2) ||| Full
-
--- TODO: Also make this conditional (on there being a working urxvt.)
 myTerminal = "LANG=en_US.utf8 urxvt"
+
+-- -
+-- -
+-- -import System.Process
+-- -import System.Exit
+-- -import System.Posix.Signals
+-- -
+-- -import XMonad.Prompt
+-- -
+-- --- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
+-- -determineModMask = do
+-- -    r <- system "xmodmap | grep '^mod3\\s*\\S' >/dev/null"
+-- -    return (if r == ExitSuccess then mod3Mask else mod1Mask)
+-- -
+-- -
+-- --- from https://github.com/sharpsaw/x-dots/blob/master/bin/%2Clock
+-- -keyToLockScreen = [ ("C-M-l", spawn "~/bin/,lock" ) ]
+-- -
+-- -keysForAltFn =
+-- -  [ ((mod1Mask, xK_F5 ), spawn "~/bin/Alt+F5")
+-- -  , ((mod1Mask, xK_F9 ), spawn "~/bin/Alt+F9") ]
+-- -
+--  main = do
+-- -    installHandler processStatusChanged Default Nothing
+-- -    myModMask <- determineModMask
+--      xmonad $ defaultConfig
+-- -        { workspaces = myWorkspaces
+-- -        , layoutHook = myLayout
+-- -        , manageHook = myManageHook <+> manageHook defaultConfig
+-- -        , modMask = myModMask
+-- +        { modMask = mod4Mask
+--          , terminal = myTerminal
+--          , normalBorderColor  = "black"
+--          , focusedBorderColor = "green"
+--          , borderWidth = 1
+--          }
+-- -        -- http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html
+-- -        -- and:
+-- -        -- http://hackage.haskell.org/packages/archive/X11/latest/doc/html/Graphics-X11-Types.html
+-- -        `additionalKeys`
+-- -        keysForAltFn
+-- -        `additionalKeysP`
+-- -        (keyToLockScreen ++ keysToMoveWindowAndSwitchWorkspaces)
+-- -
+-- -myWorkspaces = [
+-- -    "1:dev","2:web","3:irc","4:skype","5:misc",
+-- -    "6:emu","7:chrome","8:ie8","9","0","-","="]
+-- -
+-- -myManageHook = composeAll
+-- -    [ className =? "Mumble" --> doFloat
+-- -    , className =? "Pentadactyl" --> doShift "2:web" -- not matching
+-- -    , className =? "Skype" --> doShift "4:skype"
+-- -    ]
+-- -
+-- -myLayout = ThreeCol 1 (3/100) (1/2) ||| Full
+--  
+-- --- TODO: Also make this conditional (on there being a working urxvt.)
+--  myTerminal = "LANG=en_US.utf8 urxvt"
